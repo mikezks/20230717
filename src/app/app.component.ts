b4faab5a5e41358a5c06d2deb4b0e93abf942113
mikezks <23002661+mikezks@users.noreply.github.com>
2023-07-01 20:46:38 +0200

    init: services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FlightSearchComponent } from './flight-search/flight-search.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 @Component({
   standalone: true,
-  imports: [SidebarComponent, NavbarComponent],
+  imports: [SidebarComponent, NavbarComponent, FlightSearchComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
+import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [],
+  providers: [provideHttpClient()],
 });
