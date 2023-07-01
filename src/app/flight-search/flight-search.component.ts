60e0b40369e347a1a6220ad4a072aeb850ba053d
mikezks <23002661+mikezks@users.noreply.github.com>
2023-07-01 21:06:21 +0200

    init: template-driven forms

--- src/app/flight-search/flight-search.component.ts
+++ src/app/flight-search/flight-search.component.ts
@@ -5,24 +5,32 @@ import { Flight } from '../model/flight';
 import { FormsModule } from '@angular/forms';
 import { FlightService } from './flight.service';
 import { CityPipe } from '../shared/city.pipe';
+import { FlightCardComponent } from '../flight-card/flight-card.component';
 
 @Component({
   selector: 'app-flight-search',
   standalone: true,
-  imports: [CommonModule, FormsModule, CityPipe],
   templateUrl: './flight-search.component.html',
   styleUrls: ['./flight-search.component.css'],
+  imports: [CommonModule, FormsModule, CityPipe, FlightCardComponent],
 })
 export class FlightSearchComponent {
   from = 'London';
   to = 'Paris';
   flights: Array<Flight> = [];
   selectedFlight: Flight | undefined;
+  message = '';
+
+  basket: Record<number, boolean> = {
+    3: true,
+    5: true,
+  };
 
   private flightService = inject(FlightService);
 
   search(): void {
     // Reset properties
+    this.message = '';
     this.selectedFlight = undefined;
 
     this.flightService.find(this.from, this.to).subscribe({
@@ -36,6 +44,6 @@ export class FlightSearchComponent {
   }
 
   select(f: Flight): void {
-    this.selectedFlight = f;
+    this.selectedFlight = { ...f };
   }
 }
