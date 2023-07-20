f81cb293bf052f488a718e10653746e664ab61ff
mikezks <23002661+mikezks@users.noreply.github.com>
2023-07-20 08:51:20 +0200

    feat: event binding, lifecycle hooks

--- src/app/flight-search/flight-card/flight-card.component.ts
+++ src/app/flight-search/flight-card/flight-card.component.ts
@@ -1,5 +1,5 @@
 import { Flight, initialFlight } from './../../model/flight';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,12 +9,22 @@ import { CommonModule } from '@angular/common';
   templateUrl: './flight-card.component.html',
   styleUrls: ['./flight-card.component.css']
 })
-export class FlightCardComponent {
+export class FlightCardComponent implements OnInit, OnDestroy {
   // @Input() item: Flight = initialFlight;
   @Input({ required: true }) item!: Flight;
   @Input() selected = false;
+  @Output() selectedChange = new EventEmitter<boolean>();
+
+  ngOnInit(): void {
+    console.log('Flight Card INIT');
+  }
 
   toggleSelection(): void {
     this.selected = !this.selected;
+    this.selectedChange.emit(this.selected);
+  }
+
+  ngOnDestroy(): void {
+    console.log('Flight Card DESTROY');
   }
 }
