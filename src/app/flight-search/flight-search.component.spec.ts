073327e7fd94b8684006d01c622e45bc47bbbb14
mikezks <23002661+mikezks@users.noreply.github.com>
2023-07-21 10:31:22 +0200

    feat: dom testing

--- src/app/flight-search/flight-search.component.spec.ts
+++ src/app/flight-search/flight-search.component.spec.ts
@@ -1,10 +1,11 @@
 import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { FlightSearchComponent } from './flight-search.component';
 import { provideHttpClient } from '@angular/common/http';
 import { FlightService } from './flight.service';
 import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
+
 
 describe('FlightSearchComponent', () => {
   let component: FlightSearchComponent;
@@ -16,12 +17,14 @@ describe('FlightSearchComponent', () => {
       providers: [
         provideHttpClient(),
         provideHttpClientTesting(),
-        /* {
+        {
           provide: FlightService,
           useValue: {
-            find: () => of([])
+            find: () => of([
+              { id: 999, from: '', to: '', date: '', delayed: false }
+            ])
           }
-        } */
+        }
       ]
     }).compileComponents();
 
@@ -30,7 +33,28 @@ describe('FlightSearchComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should have undefined selected flights initially', () => {
-    expect(component.selectedFlight).toBeUndefined();
+  function setInput(selector: string, value: string): void {
+    const input = fixture.debugElement.query(By.css(selector)).nativeElement;
+    input.value = value;
+    input.dispatchEvent(new Event('input'));
+    tick();
+  }
+
+  it('should not have any flights loaded initially', () => {
+    expect(component.flights.length).toBe(0);
   });
+
+  it('should hava a disabled search bitton w/o params', fakeAsync(async () => {
+    tick();
+    // Set values
+    setInput('input[name=from]', '');
+    setInput('input[name=to]', '');
+
+    // Trigger change detection
+    fixture.detectChanges();
+
+    const disabled = fixture.debugElement.query(By.css('button')).nativeElement.disabled;
+
+    expect(disabled).toBeTruthy();
+  }));
 });
